e9733a39ff2bd0ce85a9ceb69a6e7e72acdf5bc6
StephanieMalakovich <malakovichstefa@gmail.com>
2024-09-20 16:56:34 +0400

    add SimpleIterationMethod

---
+++ src/main/java/functions/SimpleIterationMethod.java
@@ -0,0 +1,32 @@
+package functions;
+
+public class SimpleIterationMethod {
+    private final MathFunction phi;  // функция ϕ(x)
+    private final double tolerance;  // точность
+    private final int maxIterations; // максимальное количество итераций
+
+    public SimpleIterationMethod(MathFunction phi, double tolerance, int maxIterations) {
+        this.phi = phi;
+        this.tolerance = tolerance;
+        this.maxIterations = maxIterations;
+    }
+
+    public double solve(double initialGuess) {
+        double x = initialGuess;
+        int iterations = 0;
+
+        while (iterations < maxIterations) {
+            double nextX = phi.apply(x);
+
+            // Проверка на достижение требуемой точности
+            if (Math.abs(nextX - x) < tolerance) {
+                return nextX;
+            }
+
+            x = nextX;
+            iterations++;
+        }
+
+        throw new RuntimeException("Не удалось найти решение за " + maxIterations + " итераций.");
+    }
+}
