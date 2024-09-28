4825d170ba1d9d15edab6fafe6c14465f8b144fa
StephanieMalakovich <malakovichstefa@gmail.com>
2024-09-28 14:49:39 +0400

    added ArrayIsNotSortedException, DifferentLengthOfArraysException, InterpolationException

---
+++ src/main/java/exceptions/DifferentLengthOfArraysException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class  DifferentLengthOfArraysException extends RuntimeException {
+    public DifferentLengthOfArraysException() {
+
+    }
+    public DifferentLengthOfArraysException(String message) {
+        super(message);
+    }
+}
