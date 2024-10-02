a1cda73747ea9c20382c36c511498b21b3bdf90d
Ziorde <ilya_balakirev719@mail.ru>
2024-10-02 17:51:45 +0400

    Updated Factory and its realizations

--- src/main/java/functions/factory/ArrayTabulatedFunctionFactory.java
+++ src/main/java/functions/factory/ArrayTabulatedFunctionFactory.java
@@ -1,17 +1,11 @@
 package functions.factory;
 
 import functions.ArrayTabulatedFunction;
-import functions.StrictTabulatedFunction;
 import functions.TabulatedFunction;
 
 public class ArrayTabulatedFunctionFactory implements TabulatedFunctionFactory{
     @Override
     public TabulatedFunction create(double[] xValues, double[] yValues) {
         return new ArrayTabulatedFunction(xValues, yValues);
-    }
-
-    @Override
-    public TabulatedFunction createStrict(double[] xValues, double[] yValues) {
-        return new StrictTabulatedFunction(new ArrayTabulatedFunction(xValues, yValues));
     }
 }
--- src/main/java/functions/factory/LinkedListTabulatedFunctionFactory.java
+++ src/main/java/functions/factory/LinkedListTabulatedFunctionFactory.java
@@ -2,16 +2,10 @@ package functions.factory;
 
 import functions.LinkedListTabulatedFunction;
 import functions.TabulatedFunction;
-import functions.StrictTabulatedFunction;
 
 public class LinkedListTabulatedFunctionFactory implements TabulatedFunctionFactory {
     @Override
     public TabulatedFunction create(double[] xValues, double[] yValues) {
         return new LinkedListTabulatedFunction(xValues, yValues);
-    }
-
-    @Override
-    public TabulatedFunction createStrict(double[] xValues, double[] yValues) {
-        return new StrictTabulatedFunction(new LinkedListTabulatedFunction(xValues, yValues));
     }
 }
