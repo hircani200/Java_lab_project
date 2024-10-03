5ac82ec34b88ec3e0436d27b62a46cc81ade0bb9
Ziorde <ilya_balakirev719@mail.ru>
2024-10-03 18:17:52 +0400

    Added writeTabulatedFunction() in FunctionsIO and TabulatedFunctionFileOutput

--- src/main/java/io/FunctionsIO.java
+++ src/main/java/io/FunctionsIO.java
@@ -1,8 +1,24 @@
 package io;
 
+import functions.Point;
+import functions.TabulatedFunction;
+
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 public final class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException();
     }
 
+    static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException {
+        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+        dataOutputStream.writeInt(function.getCount());
+        for (Point point : function){
+            dataOutputStream.writeDouble(point.x);
+            dataOutputStream.writeDouble(point.y);
+        }
+        dataOutputStream.flush();
+    }
 }
