d1493a969f4a829ce3a3bb0aec165703443552af
Ziorde <ilya_balakirev719@mail.ru>
2024-10-04 19:28:17 +0400

    Created LinkedListTabulatedFunctionSerialization

---
+++ src/main/java/io/LinkedListTabulatedFunctionSerialization.java
@@ -0,0 +1,37 @@
+package io;
+
+import functions.LinkedListTabulatedFunction;
+import functions.TabulatedFunction;
+import functions.factory.LinkedListTabulatedFunctionFactory;
+import operations.TabulatedDifferentialOperator;
+
+import java.io.*;
+
+public class LinkedListTabulatedFunctionSerialization {
+    public static void main(String[] args) {
+
+
+        try(BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream("output/serialized linked list functions.bin"))){
+            TabulatedDifferentialOperator operator = new TabulatedDifferentialOperator(new LinkedListTabulatedFunctionFactory());
+
+            TabulatedFunction tabulatedFunction1 = new LinkedListTabulatedFunction(x -> 2*x + 1, -3, 3, 5);
+            TabulatedFunction derivedFunction1 = operator.derive(tabulatedFunction1);
+            TabulatedFunction derivedFunction2 = operator.derive(derivedFunction1);
+
+            FunctionsIO.serialize(bufferedOutputStream, tabulatedFunction1);
+            FunctionsIO.serialize(bufferedOutputStream, derivedFunction1);
+            FunctionsIO.serialize(bufferedOutputStream, derivedFunction2);
+            bufferedOutputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try(BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream("output/serialized linked list functions.bin"))){
+            System.out.println(FunctionsIO.deserialize(bufferedInputStream).toString());
+            System.out.println(FunctionsIO.deserialize(bufferedInputStream).toString());
+            System.out.println(FunctionsIO.deserialize(bufferedInputStream).toString());
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
