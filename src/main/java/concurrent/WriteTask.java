dd9063c2e2c81e8233b991d3b7b96390b58d19ed
Ziorde <ilya_balakirev719@mail.ru>
2024-10-06 13:37:45 +0400

    Added WriteTask

---
+++ src/main/java/concurrent/WriteTask.java
@@ -0,0 +1,22 @@
+package concurrent;
+
+import functions.TabulatedFunction;
+
+public class WriteTask implements Runnable{
+
+    private final TabulatedFunction function;
+    private final double value;
+
+    public WriteTask(TabulatedFunction function, double value) {
+        this.function = function;
+        this.value = value;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < this.function.getCount(); i++) {
+            this.function.setY(i, this.value);
+            System.out.printf("Writing for index %d complete\n", i);
+        }
+    }
+}
